2109eca35fbfe92a843fdb23fd2fa90d5e6429cd
Dmitriy Klyuzov <dmitriy.klyuzov@gmail.com>
2017-09-28 19:43:02 -0400

    added col-md-6 to messages to make validation errors same width with input container

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,17 +5,21 @@
 	<meta name="csrf-token" content="{{csrf_token()}}">
 	{{-- Compiled App.css --}}
 	<link rel="stylesheet" href="/css/app.css">
+	@yield('stylesheets')
 	{{-- Compiled App.js --}}
-	<script src="/js/app.js"></script>
 </head>
 <body>
 	@include('parts.navbar')
 	<div class="container content">
 		{{-- Messages --}}
-		@include('parts.messages')
+		<div class="col-md-6 col-md-offset-3">
+			@include('parts.messages')
+		</div>
 		{{-- Main Content --}}
 		@yield('content')
 	</div>
 	@include('parts.footer')
+	<script src="/js/app.js"></script>
+	@yield('scripts')
 </body>
 </html>
