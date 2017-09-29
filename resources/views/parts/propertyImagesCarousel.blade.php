db4fae8f12a11f944c7f3eedcaee00c4f8c6b742
Dmitriy Klyuzov <dmitriy.klyuzov@gmail.com>
2017-09-29 00:39:17 -0400

    image section moved into separate include, now carousel

---
+++ resources/views/parts/propertyImagesCarousel.blade.php
@@ -0,0 +1,39 @@
+<div id="photosCarousel" class="carousel slide" @if(count($property->photos)>1){{'data-ride="carousel"'}}@endif>
+
+	@if(count($property->photos)>1)
+	<!-- Indicators -->
+	<ol class="carousel-indicators">
+		@for($i=0; $i<count($property->photos); $i++)
+		<li data-target="#photosCarousel" data-slide-to="$i" class="@if($i==0){{'active'}}@endif"></li>
+		@endfor
+	</ol>
+	@endif
+
+	<!-- Wrapper for slides -->
+	<div class="carousel-inner">
+		@for($i=0; $i<count($property->photos); $i++)
+		@php
+			$photo = $property->photos[$i];
+		@endphp
+		<div class="item @if($i==0){{'active'}}@endif">
+			<img src="/storage/photos/{{$photo->property_id}}/{{$photo->photo}}" alt="{{$photo->title}}">
+			<div class="carousel-caption">
+				<p>{{$photo->description}}</p>
+			</div>
+		</div>
+		@endfor
+	</div>
+
+	@if(count($property->photos)>1)
+	<!-- Left and right controls -->
+	<a class="left carousel-control" href="#photosCarousel" data-slide="prev">
+		<span class="glyphicon glyphicon-chevron-left"></span>
+		<span class="sr-only">Previous</span>
+	</a>
+	<a class="right carousel-control" href="#photosCarousel" data-slide="next">
+		<span class="glyphicon glyphicon-chevron-right"></span>
+		<span class="sr-only">Next</span>
+	</a>
+	@endif
+
+</div>
