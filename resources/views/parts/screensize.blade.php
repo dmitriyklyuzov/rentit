8ce0096763ba3bac55d2876d2d35cfe0fcc4b956
Dmitriy Klyuzov <dmitriy.klyuzov@gmail.com>
2017-11-02 18:04:25 -0400

    created parts/screensize to track screen size

---
+++ resources/views/parts/screensize.blade.php
@@ -0,0 +1,6 @@
+<div class="alert alert-warning">
+	<span class="visible-xs">XS (EXTRA SMALL)</span>
+	<span class="visible-sm">SM (SMALL)</span>
+	<span class="visible-md">MD (MEDIUM)</span>
+	<span class="visible-lg">LG (LARGE)</span>
+</div>
