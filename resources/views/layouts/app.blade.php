72b1b9ba0c955552789eb15831f6ad328aa84643
Dmitriy Klyuzov <dmitriy.klyuzov@gmail.com>
2017-11-03 16:44:33 -0400

    moved flatly bootswatch theme from app.scss to /public

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,8 @@
 <head>
 	<title>@yield('title', env('APP_NAME'))</title>
 	<meta name="csrf-token" content="{{csrf_token()}}">
+	{{-- Bootstrap css --}}
+	<link rel="stylesheet" href="/css/bootstrap.min.css">
 	{{-- Compiled App.css --}}
 	<link rel="stylesheet" href="/css/app.css">
 	@yield('stylesheets')
@@ -12,8 +14,12 @@
 	@include('parts.navbar')
 	<div class="container content">
 		{{-- Messages --}}
-		<div class="col-md-6 col-md-offset-3">
-			@include('parts.messages')
+		<div class="col-xs-12">
+			<div class="col-md-6 col-md-offset-3">
+				{{-- @include('parts.screensize') --}}
+				@include('parts.messages')
+				{{-- @include('parts.test') --}}
+			</div>
 		</div>
 		{{-- Main Content --}}
 		@yield('content')
